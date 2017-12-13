2264b01eba479d7ccd3e4dacc1a8e3261e6d2360
swathiyemp <swatz2006@gmail.com>
2017-12-13 16:13:37 -0500

    Update itemcatalog.py

--- itemcatalog.py
+++ itemcatalog.py
@@ -59,7 +59,7 @@ def gconnect():
 
     try:
         # Upgrade the authorization code into a credentials object
-        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow = flow_from_clientsecrets(getClientSecrets(), scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
