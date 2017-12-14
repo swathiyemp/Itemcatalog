4a8b85e1c9c77a20f7a563a2ab475637f37f0447
swathiyemp <swatz2006@gmail.com>
2017-12-14 17:32:37 -0500

    Update itemscatalog.py

--- itemscatalog.py
+++ itemscatalog.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from sqlalchemy import desc
 
 Base = declarative_base()
 
